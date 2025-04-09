d4ce40bde94e66341d2b2b8e8ce2e1d8c900f665
ZAIN SHEIKH <105550667+Zainshk64@users.noreply.github.com>
2025-04-09 20:50:05 +0500

    Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -38,7 +38,7 @@ const Hero = () => {
           </div>
           <div className="flex flex-wrap justify-center md:justify-start gap-4">
             <a
-              href="../assets/Zain_Ejaz_Frontend_Developer_Resume.pdf"
+               href="../../public/Zain_Ejaz_Frontend_Developer_Resume.pdf"
               target='_blank'
               className="px-6 py-3 bg-cyan-500 text-white rounded-full font-semibold hover:bg-cyan-600 transition"
             >
